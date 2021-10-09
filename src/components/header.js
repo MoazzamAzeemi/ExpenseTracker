86260de36aa2ae105f26dacead16cefca89448e6
MoazzamAzeemi <38378900+MoazzamAzeemi@users.noreply.github.com>
2021-10-09 03:30:21 -0700

    initial structure of expense tracker app

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="App App-header" >
+            <h4>
+                Developed by<br />
+                <a
+                    className="App-link"
+                    href="https://github.com/MoazzamAzeemi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Moazzam Azeemi
+                </a>
+            </h4>
+        </header>
+    );
+}
+
+export default Header;
