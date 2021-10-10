b3b21ed071d16138da95c6013b8236ca261b4fe5
MoazzamAzeemi <38378900+MoazzamAzeemi@users.noreply.github.com>
2021-10-10 06:48:30 -0700

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+
 import Balance from './components/Balance';
 import TransactionHistory from './components/TransactionHistory';
 import NewTransaction from './components/NewTransaction';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
     <div className="tc f3 bg-light-blue pa3 br4">
-      <Header />
+      
       <Balance balance={50-50} />
       <TransactionHistory />
-      <NewTransaction />      
+      <NewTransaction />
+      <Header />
     </div>
     </>
   );
