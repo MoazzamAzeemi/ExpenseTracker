e5f8204ba85ac29c0948c5751831ec57e2954e3b
MoazzamAzeemi <38378900+MoazzamAzeemi@users.noreply.github.com>
2021-10-10 07:13:24 -0700

    removed components folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
-
-import Balance from './components/Balance';
-import TransactionHistory from './components/TransactionHistory';
-import NewTransaction from './components/NewTransaction';
-import Header from './components/Header';
+import Header from './Header';
+import Balance from './Balance';
+import TransactionHistory from './TransactionHistory';
+import NewTransaction from './NewTransaction';
 
 function App() {
   return (
     <>
     <div className="tc f3 bg-light-blue pa3 br4">
-      
+      <Header />
       <Balance balance={50-50} />
       <TransactionHistory />
       <NewTransaction />
-      <Header />
+      
     </div>
     </>
   );
